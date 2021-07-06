de7fdaa33229c0927a62e782aee123e018c6fef8
maryrenirajaian <mary.rajaian@instana.com>
2021-07-06 14:12:05 +0200

    add annotations to correlate spans

--- instana-java-sdk-sample/custom-http-sample/src/main/java/com/instana/sample/App.java
+++ instana-java-sdk-sample/custom-http-sample/src/main/java/com/instana/sample/App.java
@@ -9,7 +9,10 @@ import com.instana.sample.http.custom.CustomHttpFramework;
 import com.instana.sample.http.jetty.JettyServer;
 import com.instana.sample.http.jetty.NameServlet;
 import com.instana.sample.http.jetty.RootServlet;
+import com.instana.sdk.annotation.Span;
+import com.instana.sdk.support.SpanSupport;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class App {
@@ -19,7 +22,6 @@ public class App {
 
   public static void main(String[] args) throws Exception {
     CustomHttpFramework framework = new CustomHttpFramework();
-    CustomHttpClient customHttpClient = new CustomHttpClient();
     final Thread thread = new Thread(() -> {
       try {
         JettyServer.run(8080, "/", RootServlet.class);
@@ -35,6 +37,16 @@ public class App {
     thread.start();
     framework.awaitServerStartup();
     Thread.sleep(Duration.ofMinutes(1).toMillis());
+    createClient();
+  }
+
+  @Span(value = "scheduled-demo")
+  private static void createClient() throws IOException {
+    SpanSupport.annotate("batch.job", "demo");
+    SpanSupport.annotate("service", "demo-service");
+    SpanSupport.annotate("endpoint", "demo-endpoint");
+    SpanSupport.annotate("call.name", "demo-call");
+    CustomHttpClient customHttpClient = new CustomHttpClient();
     customHttpClient.makeCall(HOST, PORT, GREETING);
   }
 }
--- instana-java-sdk-sample/custom-http-sample/src/main/java/com/instana/sample/http/custom/CustomHttpClient.java
+++ instana-java-sdk-sample/custom-http-sample/src/main/java/com/instana/sample/http/custom/CustomHttpClient.java
@@ -1,6 +1,8 @@
 package com.instana.sample.http.custom;
 
+import com.instana.sample.App;
 import com.instana.sdk.annotation.Span;
+import com.instana.sdk.support.SpanSupport;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,12 +12,14 @@ import java.net.Socket;
 
 public class CustomHttpClient {
 
-  @Span(type = Span.Type.EXIT, value = "http-client")
+  @Span(type = Span.Type.EXIT, value = "custom-http-client")
   public void makeCall(String host, int port, String path) throws IOException {
     try (Socket socket = new Socket(host, port);
          PrintWriter outputStream = new PrintWriter(socket.getOutputStream());
          BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
       outputStream.println("GET " + path + " HTTP/1.1");
+      outputStream.println(SpanSupport.TRACE_ID + ": " + SpanSupport.traceId());
+      outputStream.println(SpanSupport.SPAN_ID + ": " + SpanSupport.spanId());
       outputStream.println("");
       outputStream.flush();
       reader.lines().forEach(System.out::println);
