5722c07424bc4d95b21e30a57cccafc28fb843b7
Atakan Toker <77298161+atakantoker@users.noreply.github.com>
2026-01-30 10:17:59 +0100

    Update License Header (#34)
    Co-authored-by: Atakan Toker <Atakan.Toker@ibm.com>

--- instana-java-sdk-sample/custom-scheduler-sample/src/main/java/com/instana/sample/App.java
+++ instana-java-sdk-sample/custom-scheduler-sample/src/main/java/com/instana/sample/App.java
@@ -1,8 +1,7 @@
 /*
- * (c) Copyright IBM Corp. 2021
+ * (c) Copyright IBM Corp. 2026
  * (c) Copyright Instana Inc.
  */
-
 package com.instana.sample;
 
 import com.instana.sample.http.jetty.HelloWorldServlet;
